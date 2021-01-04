be7b433fff7f642feea754c6b6fe61c4f32fd828
Emerson Duarte <emerson.duarte@zallpy.com>
2021-01-04 10:53:53 -0300

    finishing

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import routes from './routes/index';
 
-const app = express()
+const app = express();
 
+app.use(express.json());
 app.get('/', (request, response) => {
   return response.json({ message: 'Funcionando' })
-})
+});
 
 app.listen(3333, () => {
   console.log('Server run on port 3333')
-})
+});
