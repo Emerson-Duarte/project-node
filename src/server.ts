589b2ab87e1a0586534fead670bb73739b777319
Emerson Duarte <emerson.duarte@zallpy.com>
2021-01-04 11:48:56 -0300

    testing routes and validate appointment in same date

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
-import routes from './routes/index';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.get('/', (request, response) => {
-  return response.json({ message: 'Funcionando' })
-});
+
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server run on port 3333')
+  console.log('Server run on port 3333');
 });
